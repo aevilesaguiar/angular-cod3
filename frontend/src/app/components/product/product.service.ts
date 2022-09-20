e336ee023881b2607badd5a2dceb474f6f80f8b5
aevilesaguiar <aeviles@hotmail.com>
2022-09-19 22:10:32 -0300

    finalizar

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product.module';
-import { from, Observable } from 'rxjs';
+import { Observable , EMPTY} from 'rxjs';
+import { map, catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,20 +14,33 @@ export class ProductService {
 
   constructor( private snackBar: MatSnackBar, private http: HttpClient) { }
 
-  showMessage( msg: string):void{
+  showMessage( msg: string, isError: boolean=false):void{
     this.snackBar.open(msg, 'X',{
       duration:3000,
       horizontalPosition:"right",
-      verticalPosition: "top"
+      verticalPosition: "top",
+      panelClass:isError ?['.msg-error']: ['msg-sucess'] //se der erro ele mostra essa classe senão mostra sucesso
+                                                        //como isError é false ele vai continuar mandando sucesso
     })
   }
 
   //responsável por inserir no nosso backend
   create(product: Product):Observable<Product> {
     //mandar uma requisição http para o backend
-    return this.http.post<Product>(this.baseUrl, product)
-
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
+
+
+  errorHandler(e:any): Observable<any>{
+    console.log(e);
+    this.showMessage('Ocorreu um erro!', true);
+    //Esse método retorna um Observable vazio tipo EMPTY,
+    return EMPTY;
+  }
+
 
   //ler uma lista de produtos cadastrados no backend
   read(): Observable<Product[]>{
